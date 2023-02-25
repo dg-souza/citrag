c204d2110eeeb8210f11f553ce743d821dd7aeb7
davisg <davi.gama@triadsystems.com.br>
2023-02-25 14:02:23 -0300

    commit changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  Container,
+  Content
+} from './style/GlobalStyle'
+
+import PlayertList from './components/PlayerList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Welcome to Citrag</h1>
+      <Content>
+        <PlayertList />
+      </Content>
+    </Container>
   );
 }
 
