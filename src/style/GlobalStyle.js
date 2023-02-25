93233c19cf568a0001c7f46f7936803c3b76d5c7
davisg <davi.gama@triadsystems.com.br>
2023-02-25 13:59:36 -0300

    GlobalStyle Created

---
+++ src/style/GlobalStyle.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Content = styled.div`
+    display: flex;
+    flex-direction: row;
+    min-width: 1000px;
+    min-height: 600px;
+    background: red;
+`
